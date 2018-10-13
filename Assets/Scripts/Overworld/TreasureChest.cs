adb2bc7a1f0524a5087a0cd840f0020ff6c22aa3
Leo Grandinetti <leolegrande@gmail.com>
2018-10-13 19:55:55 -0400

    Updated overworld

---
+++ Assets/Scripts/Overworld/TreasureChest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour {
+
+    public BoxCollider2D bc;
+    public List<GameObject> items;
+
+	// Use this for initialization
+	void Start () {
+        bc = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "player")
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                print("opening chest");
+            }
+        }
+    }
+
+    public void giveItems()
+    {
+        
+    }
+}
