170052fc356135deac9a21b3c2a17a34e2b78312
Leo Grandinetti <leolegrande@gmail.com>
2018-10-13 03:28:55 -0400

    Added discarding mechanic

---
+++ Assets/Scripts/DetectClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DetectClick : MonoBehaviour,IPointerClickHandler {
+
+    public Card card;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            card.use();
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            card.discard();
+        }
+    }
+}
